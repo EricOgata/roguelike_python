1ef6e1cab965b4189684e3cbd9a89b5190893f73
Eric Ogata <ericogata@ymail.com>
2019-02-24 18:02:49 -0300

    feat(part4): part 4 finalizada

--- README.md
+++ README.md
@@ -1,10 +1,26 @@
 # roguelike_python
 a small roguelike game in python
+***
 
 based in this tutorial:
 [Tdl + Python 3](http://rogueliketutorials.com/tdl/1)
 
-Command to install pip
+***
+### Command to install pip
 ```
 python -m pip install tcod
 ```
+
+***
+### Images:
+![FOV-example](https://media.giphy.com/media/1NUP582Ll1pIGrWXAl/source.gif)
+
+***
+### Color-Palettes:
+#### 8-bit old-school pallete:
+| Hex     | RGBA                | RGB              |
+|---------|---------------------|------------------|
+| #04202a | rgba(4,32,42,1)     | rgb(4,32,42)     |
+| #2e4040 | rgba(46,64,64,1)    | rgb(46,64,64)    |
+| #5a6e65 | rgba(90,110,101,1)  | rgb(90,110,101)  |
+| #c7cfc6 | rgba(199,207,198,1) | rgb(199,207,198) |
--- render_functions.py
+++ render_functions.py
@@ -1,16 +1,26 @@
-def render_all(con, entities, game_map, root_console, screen_width, screen_height, colors):
-    # Draw all the tiles in the game map:
-    for x, y in game_map:
-        wall = not game_map.transparent[x, y];
+def render_all(con, entities, game_map, fov_recompute, root_console, screen_width, screen_height, colors):
+    if fov_recompute: 
+        # Draw all the tiles in the game map:
+        for x, y in game_map:
+            wall = not game_map.transparent[x, y];
 
-        if wall:
-            con.draw_char(x, y, "#", fg=colors.get('dark_wall'), bg=None);
-        else:
-            con.draw_char(x, y, None, fg=None, bg=colors.get('dark_ground'));            
+            if game_map.fov[x, y]:
+                if wall:
+                    con.draw_char(x, y, None, fg=None, bg=colors.get('light_wall'));
+                else:
+                    con.draw_char(x, y, None, fg=None, bg=colors.get('light_ground'));
+                
+                game_map.explored[x][y] = True;
+                
+            elif game_map.explored[x][y]:
+                if wall:
+                    con.draw_char(x, y, None, fg=None, bg=colors.get('dark_wall'));
+                else:
+                    con.draw_char(x, y, None, fg=None, bg=colors.get('dark_ground')); 
 
     # Draw all entities in the list
     for entity in entities:
-        draw_entity(con, entity);
+        draw_entity(con, entity, game_map.fov);
     
     root_console.blit(con, 0, 0, screen_width, screen_height, 0, 0);
 
@@ -18,8 +28,9 @@ def clear_all(con, entities):
     for entity in entities:
         clear_entity(con, entity);
 
-def draw_entity(con, entity):
-    con.draw_char(entity.x, entity.y, entity.char, entity.color, bg=None);
+def draw_entity(con, entity, fov):
+    if fov[entity.x, entity.y]:
+        con.draw_char(entity.x, entity.y, entity.char, entity.color, bg=None);
 
 def clear_entity(con, entity):
     # erase the character that represents this object
